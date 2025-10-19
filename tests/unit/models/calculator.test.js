6120aafec0cf1dc624c5edd65bd0a817d015039f
GustavoMont <lgmluisgm@gmail.com>
2025-10-19 14:37:00 -0300

    test: add `jest` and config test running

---
+++ tests/unit/models/calculator.test.js
@@ -0,0 +1,10 @@
+import calculator from "../../../models/calculator";
+
+describe("Calculator", () => {
+  describe("sum", () => {
+    test("Sum multiple numbers", () => {
+      const result = calculator.sum(1, 2, 3);
+      expect(result).toBe(6);
+    });
+  });
+});
